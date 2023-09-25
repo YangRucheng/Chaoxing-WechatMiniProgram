c70646a252eee3285c3d52cf5dc2757424e288b6
Sakana <admin@yangrucheng.top>
2023-09-25 17:51:14 +0800

    强制更新小程序到最新版

--- pages/tabbar-package/login/login.js
+++ pages/tabbar-package/login/login.js
@@ -18,6 +18,24 @@ Page({
     },
 
     onLoad(options) {
+        this.login(options);
+
+        const updateManager = wx.getUpdateManager();
+        updateManager.onUpdateReady(() => {
+            wx.showModal({
+                title: '更新提示',
+                content: '新版本已准备好，即将重启应用',
+                confirmText: '立即重启',
+                showCancel: false,
+            }).then(res => {
+                if (res.confirm) {
+                    updateManager.applyUpdate();
+                }
+            })
+        })
+    },
+
+    login(options) { // 登录
         wx.login()
             .then(res => {
                 this.showLoading("正在登录")
@@ -33,7 +51,7 @@ Page({
                             util.setStorage('vip', res.data.vip);
                             util.setStorage('uid', res.data.uid);
                             util.setStorage('token', res.data.token);
-                            util.setStorage('inviteNum',res.data.inviteNum);
+                            util.setStorage('inviteNum', res.data.inviteNum);
                         } else
                             throw res.msg;
                     })
